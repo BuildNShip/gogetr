3775b587adf93fa9b40c428a39b9d2f2b608dd35
shamila p <lulushamila@gmail.com>
2023-05-04 15:17:34 +0530

    task creation done

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,127 +1,64 @@
-// import React from "react"
-// import "./home.css"
-// import { FiCopy } from "react-icons/fi"
-
-
-// const Home = () => {
-  
-//   return (
-//     <div className="background_container">
-//       <div className="main_container">
-//         <div className="navbar_header">
-//           {/* <p className={styles.brand_text}>IDfy</p> */}
-//         </div>
-//         <div className="first_view_container">
-//           <div className="first_view">
-//             <p className="first_view_heading">
-//               Generate Unique UUID with Ease For Your App using{" "}
-//               <span>IDfy</span>.
-//             </p>
-//             <p className="first_view_tagline">
-//               IDfy is a simple and easy to use tool that generates unique ID for
-//               your app.
-//             </p>
-
-//             <div className="first_view_uuid">
-              
-//             </div>
-
-//             <div className="first_view_button_container">
-//               <button
-//                 className="first_view_button_one"
-//               >
-//                 Copy and Generate
-//               </button>
-//               <button
-//                 className="first_view_button"
-                
-//               >
-//                 Generate New
-//               </button>
-//             </div>
-            
-//           </div>
-//         </div>
-//         <div></div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Home
-
-
-
-
-
-
-
-
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Footer from "../Footer/footer";
 
 const Home = () => {
- 
+    const [showInput, setShowInput] = useState(false);
+    const [taskName, setTaskName] = useState("");
+
+    const handleCreateTask = () => {
+        setShowInput(true);
+    };
+
+    const handleTaskNameChange = (event) => {
+        setTaskName(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Do something with the task name, like add it to a list of tasks
+        setShowInput(false);
+        setTaskName("");
+    };
+
+    const renderTaskInput = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <input className="create-task-input" type="text" value={taskName} onChange={handleTaskNameChange} />
+                <button className="create-task-button" type="submit">Add Task</button>
+            </form>
+        );
+    };
+
     return (
         <div className="background_container">
-                <div className="first_view_container">
-                    <div className="first_view">
-                        <div className="first_view_texts">
-                            <p className="fv_heading">
-                             Unlock project success with <span> Gogetr's </span> bite-sized approach!
-                            </p>
-
-                            <p className="fv_tagline">
-                            With Gogetr, you can say goodbye to overwhelming project steps and hello to a more manageable workflow. By breaking down your project into smaller, more achievable tasks, Gogetr helps you stay focused and motivated, one step at a time.
-                            </p>
-
-                            <button className="start_button" >Show Tasks</button>
-                               
-                        </div>
+            <div className="first_view_container">
+                <div className="first_view">
+                    <div className="first_view_texts">
+                        {showInput ? (
+                            renderTaskInput()
+                        ) : (
+                            <>
+                                <p className="fv_heading">
+                                    Unlock project success with <span> Gogetr's </span> bite-sized approach!
+                                </p>
+                                <p className="fv_tagline">
+                                    With Gogetr, you can say goodbye to overwhelming project steps and hello to a more manageable workflow. By breaking down your project into smaller, more achievable tasks, Gogetr helps you stay focused and motivated, one step at a time.
+                                </p>
+                                <div className="fv_buttons">
+                                    <button className="start_button">Show Tasks</button>
+                                    <button className="fv_button" onClick={handleCreateTask}>
+                                        Create Tasks
+                                    </button>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
-                <Footer/>
+            </div>
+            <Footer />
         </div>
     );
 };
 
 export default Home;
-
-
-
-
-
-
-
-
-// import React from "react"
-// import "./Home.css"
-
-
-// const Home = () => {
- 
-//   return (
-//     <div className="background_container">
-//       <div className="main_container">
-//         <div className="first_view_container">
-//           <div className="first_view">
-//             <p className="first_view_heading">
-//               Generate Unique UUID with Ease For Your App using{" "}
-//               <span>IDfy</span>.
-//             </p>
-//             <p className="first_view_tagline">
-//               IDfy is a simple and easy to use tool that generates unique ID for
-//               your app.
-//             </p>
-//             <button className="first_view_button_one">
-//                 Show Tasks
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Home
